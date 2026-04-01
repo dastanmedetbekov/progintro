8faa6f8484e274679a1c451141711a6048cf05c8
Dastan Medetbekov <dastanmedetbekovv@gmail.com>
2026-04-01 16:24:55 +0600

    Auto-backup 2026-04-01

--- 04.C/4-12/main.c
+++ 04.C/4-12/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 
-char find(char *s, char *dest) {
+char* find(char *str, char *dest) {
+    if (*dest == '\0') {
+        return (char*)str;
+    }
 
+    for(; *str; str++) {
+        int count = 0;
+
+        char *s = str;
+        char *t = dest;
+
+        while(*s && *t && *s == *t) {
+            s++;
+            t++;
+        }
+
+        if (*t == '\0') {
+            return (char*)str;
+        }
+    }
+    return NULL;
 }
 
 int main(int argc, char **argv) {
@@ -9,8 +28,28 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    for (int i = 3; i < argc; i++) {
-        find(argv[i], argv[2]);
+    for (int i = 2; i < argc; i++) {
+        char* tester;
+        tester = find(argv[i], argv[1]);
+        if (tester != NULL) {
+            printf("%s\n", argv[i]);
+        }
+    }
+    printf("=+=+=+=\n");
+
+    char *sub = argv[1];
+
+    for (int i = 2; i < argc; i++) {
+        int count = 0;
+        char *pos = argv[i];
+        while ((pos = find(pos, sub)) != NULL) {
+            count++;
+            pos++;
+        }
+
+        if (count > 0) {
+            printf("%s: %d\n", argv[i], count);
+        }
     }
 
 
