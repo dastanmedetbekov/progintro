dc321350c11f788719a203ec58ff934a609eea2e
Dastan Medetbekov <dastanmedetbekovv@gmail.com>
2026-05-12 09:26:49 +0600

    Added 43

---
+++ 04.C/4-18/43.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 2026-05-10. Started working at this task.
+// Need to write like this, vertical
+//
+// h w I w o t
+// e o w o n h
+// l r i r i
+// l l l k s
+// o d l
+
+#define WITH_SPACE 0
+
+int main() {
+    return 1;
+}
