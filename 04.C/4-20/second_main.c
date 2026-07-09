6085ae9914d4268d843c110cdf4e359803eda074
Dastan Medetbekov <dastanmedetbekovv@gmail.com>
2026-07-10 00:26:02 +0600

    hello

--- 04.C/4-20/second_main.c
+++ 04.C/4-20/second_main.c
@@ -1,26 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void das_malloc(char **str_ptr, size_t *capacity_ptr, size_t current_length) {
-    *capacity_ptr = (*capacity_ptr) * 2;
-    char *new_str = malloc(*capacity_ptr * sizeof(char));
-    if (new_str == NULL) {
-        return 1;
+static char *grow_char_buffer(char *old_buf, size_t old_cap, size_t new_cap)
+{
+    char *new_buf = malloc(new_cap * sizeof(char));
+    if (new_buf == NULL) {
+        fprintf(stderr, "ERROR\n");
+        exit(1);
     }
-
-    memcpy(new_str, *str_ptr, current_length);
-    free(*str_ptr);
-    *str_ptr = new_str;
+    size_t i;
+    for (i = 0; i < old_cap; i++) {
+        new_buf[i] = old_buf[i];
+    }
+    free(old_buf);
+    return new_buf;
 }
 
-void print_reverse(char **words, size_t count) {
-    for (size_t i = count; i > 0; i--) {
-        printf("%s ", words[i - 1]);
-        free(words[i - 1]);
+static char **grow_words_array(char **old_arr, size_t old_cap, size_t new_cap)
+{
+    char **new_arr = malloc(new_cap * sizeof(char *));
+    if (new_arr == NULL) {
+        fprintf(stderr, "ERROR\n");
+        exit(1);
+    }
+    size_t i;
+    for (i = 0; i < old_cap; i++) {
+        new_arr[i] = old_arr[i];
+    }
+    free(old_arr);
+    return new_arr;
+}
+
+static void finish_word(char **word_buf, size_t *word_len, size_t *word_cap,
+                         char ***words, size_t *words_count, size_t *words_cap)
+{
+    if (*word_len + 1 > *word_cap) {
+        size_t new_cap = (*word_cap == 0) ? 1 : (*word_cap) * 2;
+        *word_buf = grow_char_buffer(*word_buf, *word_cap, new_cap);
+        *word_cap = new_cap;
+    }
+    (*word_buf)[*word_len] = '\0';
+
+    if (*words_count + 1 > *words_cap) {
+        size_t new_wcap = (*words_cap == 0) ? 1 : (*words_cap) * 2;
+        *words = grow_words_array(*words, *words_cap, new_wcap);
+        *words_cap = new_wcap;
+    }
+    (*words)[*words_count] = *word_buf;
+    (*words_count)++;
+
+    *word_buf = NULL;
+    *word_len = 0;
+    *word_cap = 0;
+}
+
+static void flush_line(char **words, size_t words_count)
+{
+    size_t i;
+    for (i = words_count; i > 0; i--) {
+        printf("%s", words[i - 1]);
+        if (i - 1 > 0) {
+            printf(" ");
+        }
     }
     printf("\n");
+
+    for (i = 0; i < words_count; i++) {
+        free(words[i]);
+    }
 }
 
-int main() {
+int main(void)
+{
+    int c;
+
+    char *word_buf = NULL;   
+    size_t word_len = 0;     
+    size_t word_cap = 0;     
+
+    char **words = NULL;     
+    size_t words_count = 0;
+    size_t words_cap = 0;
+
+    int in_word = 0;         
+
+    while ((c = getchar()) != EOF) {
+
+        if (c == '\n') {
+            if (in_word) {
+                finish_word(&word_buf, &word_len, &word_cap,
+                            &words, &words_count, &words_cap);
+                in_word = 0;
+            }
+
+            flush_line(words, words_count);
+            free(words);
+
+            words = NULL;
+            words_count = 0;
+            words_cap = 0;
+        }
+        else if (c == ' ' || c == '\t') {
+            if (in_word) {
+                finish_word(&word_buf, &word_len, &word_cap,
+                            &words, &words_count, &words_cap);
+                in_word = 0;
+            }
+        }
+        else {
+            if (word_len + 1 > word_cap) {
+                size_t new_cap = (word_cap == 0) ? 1 : word_cap * 2;
+                word_buf = grow_char_buffer(word_buf, word_cap, new_cap);
+                word_cap = new_cap;
+            }
+            word_buf[word_len] = (char)c;
+            word_len++;
+            in_word = 1;
+        }
+    }
+
+    if (in_word) {
+        finish_word(&word_buf, &word_len, &word_cap,
+                    &words, &words_count, &words_cap);
+    }
+    if (words_count > 0) {
+        flush_line(words, words_count);
+        free(words);
+    }
+
     return 0;
 }
