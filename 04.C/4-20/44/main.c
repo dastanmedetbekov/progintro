dbb706ff3928c1770af376fe56847341ad5f3d66
Dastan Medetbekov <dastanmedetbekovv@gmail.com>
2026-07-10 22:55:39 +0600

    solved 4-20

---
+++ 04.C/4-20/44/main.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static char *grow_char_buffer(char *old_buf, size_t old_cap, size_t new_cap)
+{
+    char *new_buf = malloc(new_cap * sizeof(char));
+    if (new_buf == NULL) {
+        exit(1);
+    }
+    size_t i;
+    for (i = 0; i < old_cap; i++) {
+        new_buf[i] = old_buf[i];
+    }
+    free(old_buf);
+    return new_buf;
+}
+
+static char **grow_words_array(char **old_arr, size_t old_cap, size_t new_cap)
+{
+    char **new_arr = malloc(new_cap * sizeof(char *));
+    if (new_arr == NULL) {
+        exit(1);
+    }
+
+    size_t i;
+    for (i = 0; i < old_cap; i++) {
+        new_arr[i] = old_arr[i];
+    }
+    free(old_arr);
+    return new_arr;
+}
+
+static void finish_word(char **word_buf, size_t *word_len, size_t *word_cap, char ***words, size_t *words_count, size_t *words_cap)
+{
+    if (*word_len + 1 > *word_cap) {
+        size_t new_cap = (*word_cap == 0) ? 1 : (*word_cap) * 2;
+        *word_buf = grow_char_buffer(*word_buf, *word_cap, new_cap);
+        *word_cap = new_cap;
+    }
+    (*word_buf)[*word_len] = '\0';
+
+    if (*words_count + 1 > *words_cap) {
+        size_t new_wcap = (*words_cap == 0) ? 1 : (*words_cap) * 2;
+        *words = grow_words_array(*words, *words_cap, new_wcap);
+        *words_cap = new_wcap;
+    }
+    (*words)[*words_count] = *word_buf;
+    (*words_count)++;
+    
+    *word_buf = NULL;
+    *word_len = 0;
+    *word_cap = 0;
+}
+
+// should write this here
+size_t my_strlen(const char *str); 
+
+static void flush_digits_line(char **words, size_t words_count)
+{
+    printf("\nRESULT: ");
+    size_t max_len = 0;
+
+    for (size_t i = 0; i < words_count; i++) {
+        if (my_strlen(words[i]) > max_len) {
+            max_len = my_strlen(words[i]);
+        }
+    }
+    for (size_t i = 0; i < words_count; i++) {
+        if (my_strlen(words[i]) == max_len) {
+            printf("%s ", words[i]);
+        }
+    }
+    printf("\n \n");
+    
+}
+
+
+size_t my_strlen(const char *str) {
+    size_t length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+
+
+
+int main(void)
+{
+    int c;
+
+    char *word_buf = NULL;   
+    size_t word_len = 0;     
+    size_t word_cap = 0;     
+
+    char **words = NULL;     
+    size_t words_count = 0;
+    size_t words_cap = 0;
+
+    int in_word = 0;         
+
+    while ((c = getchar()) != EOF) {
+
+        if (c == '\n') {
+            if (in_word) {
+                finish_word(&word_buf, &word_len, &word_cap,
+                            &words, &words_count, &words_cap);
+                in_word = 0;
+            }
+
+            flush_digits_line(words, words_count);
+            free(words);
+
+            words = NULL;
+            words_count = 0;
+            words_cap = 0;
+        }
+        else if (!(c >= '0' && c <= '9')) {
+            if (in_word) {
+                finish_word(&word_buf, &word_len, &word_cap,
+                            &words, &words_count, &words_cap);
+                in_word = 0;
+            }
+        }
+        else {
+            if (word_len + 1 > word_cap) {
+                size_t new_cap = (word_cap == 0) ? 1 : word_cap * 2;
+                word_buf = grow_char_buffer(word_buf, word_cap, new_cap);
+                word_cap = new_cap;
+            }
+            word_buf[word_len] = (char)c;
+            word_len++;
+            in_word = 1;
+        }
+    }
+
+    if (in_word) {
+        finish_word(&word_buf, &word_len, &word_cap,
+                    &words, &words_count, &words_cap);
+    }
+    if (words_count > 0) {
+        flush_digits_line(words, words_count);
+        free(words);
+    }
+
+    return 0;
+} 
