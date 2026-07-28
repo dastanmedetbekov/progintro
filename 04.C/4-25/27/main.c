94cae8f0ab29d5a9e2a02bf8b3c1b9d98d69939d
Dastan Medetbekov <dastanmedetbekovv@gmail.com>
2026-07-28 20:44:50 +0600

    hello

--- 04.C/4-25/27/main.c
+++ 04.C/4-25/27/main.c
@@ -8,16 +8,30 @@ void sleepyjo() {
 int main() {
     initscr();
     curs_set(0); 
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
 
     int y = LINES / 2; 
     int max_x = COLS - 2;
 
     for (int pass = 0; pass < 3; pass++) {
+        int hell = 0;
+
         for (int x = 0; x <= max_x; x++) {
             clear();
             mvprintw(y, x, "*");
             refresh();
             sleepyjo();
+            if (getch() != ERR) {
+                hell = 1;
+                break;
+            }
+        }
+
+        if (hell == 1) {
+            break;
         }
 
         for (int x = max_x; x >= 0; x--) {
@@ -25,6 +39,13 @@ int main() {
             mvprintw(y, x, "*");
             refresh();
             sleepyjo();
+            if (getch() != ERR) {
+                hell = 1;
+                break;
+            }
+        }
+        if (hell == 1) {
+            break;
         }
     }
 
